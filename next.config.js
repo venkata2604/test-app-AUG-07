f1087951aa0fb1918e518ec18260e2b8f74b6d42
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2026-08-10 21:05:38 -0500

    Fix nested pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const repo = "test-app-AUG-07";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 module.exports = nextConfig;
