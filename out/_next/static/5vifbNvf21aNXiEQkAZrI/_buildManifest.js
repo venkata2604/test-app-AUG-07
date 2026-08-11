baaa08765498296278ac6cc6ccc48dbd43734071
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2026-08-10 21:07:08 -0500

    new build

--- out/_next/static/5vifbNvf21aNXiEQkAZrI/_buildManifest.js
+++ out/_next/static/5vifbNvf21aNXiEQkAZrI/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/test-app-AUG-07//_next/:path+",
+        "destination": "/test-app-AUG-07/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
