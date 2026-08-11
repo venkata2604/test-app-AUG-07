bcbca88ba405146f21103cf2b26bab30f50ee151
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2026-08-10 21:40:46 -0500

    add youtube vido to sample page 1

--- src/app/sample-one/page.tsx
+++ src/app/sample-one/page.tsx
@@ -16,6 +16,32 @@ export default function SampleOnePage() {
             This is the first sample page for checking basic navigation.
           </Typography>
 
+          <Box
+            sx={{
+              position: "relative",
+              width: "100%",
+              aspectRatio: "16 / 9",
+              overflow: "hidden",
+              borderRadius: 1,
+              bgcolor: "grey.100",
+            }}
+          >
+            <Box
+              component="iframe"
+              src="https://www.youtube.com/embed/XjeRTTaBP4w?start=83"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              sx={{
+                position: "absolute",
+                inset: 0,
+                width: "100%",
+                height: "100%",
+                border: 0,
+              }}
+            />
+          </Box>
+
           <Stack direction="row" spacing={2}>
             <Button component={Link} href="/" variant="outlined">
               Home
